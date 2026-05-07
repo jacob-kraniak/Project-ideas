6ababb260aa738fb437690fc61dc9e3b26f30fe8
Jacob Kraniak <113630934+jacob-kraniak@users.noreply.github.com>
2026-05-06 20:41:26 -0400

    Delete Tesla Public Charging Dashboard directory
    moved to new directory

--- Tesla Public Charging Dashboard/installation.py
+++
@@ -1,5 +0,0 @@
-# Inside tesla-charging-dashboard/
-python -m venv .venv
-source .venv/bin/activate    # or .venv\Scripts\activate on Windows
-pip install -r requirements.txt
-cp .env.example .env         # then edit with your token
